3e82df49d77e1bcd3a855409eb8728859c1f0cff
Olga, Paul & Veny T <tchistopolskii@gmail.com>
2018-03-19 17:58:42 -0700

    Encode variables to hide them from GoogleTranslate

--- poToCsv.py
+++ poToCsv.py
@@ -12,6 +12,18 @@ def stripEnds(line):
     line = re.sub(r'^"|"$', '', line)
     return line
 
+def exractVariables(line):
+    line_vars = {}
+    pattern = re.compile(r"%\(\w+\)s")
+    items = re.findall(pattern, line)
+    for i, item in enumerate(items):
+        code = 'xxx' + str(i) + 'xxx'
+        line = line.replace(item, code)
+        line_vars[ code ] = item
+    var_str = ', '.join(reversed(['%s=%s' % (c, v) for (c, v) in line_vars.items()]))
+    return [line, var_str]
+
+
 string = ""
 with open("locale/en_US/LC_MESSAGES/django.po") as f, open('translations.csv', 'w') as c:
     writer = csv.writer(c)
@@ -20,9 +32,10 @@ with open("locale/en_US/LC_MESSAGES/django.po") as f, open('translations.csv', '
             continue
 
         if(re.search('msgstr', line)): ## Line index found
-            print (string)
             string = stripEnds(string)
-            writer.writerow((i, string))
+            string,var_str = exractVariables(string)
+            print (string, '>>>', var_str)
+            writer.writerow((i, var_str, string))
         elif(line.startswith('msgid')):
             line = line.replace('msgid "', '')
             line = stripEnds(line)
